368879800b2a540ab69cd648d9b82dd7de50252e
Charmi <charmi@aum.bz>
2022-10-14 09:29:46 +0530

    14th OCT. first commit

--- Javascript/ajax.js
+++ Javascript/ajax.js
@@ -1,7 +1,7 @@
 function myAjaxFunc()
 {
 	const xhttp = new XMLHttpRequest();
-	xhttp.open('GET', 'ajaxtext.php');
+	xhttp.open('GET', 'ajaxtext.txt');
 	xhttp.send();
 	xhttp.onload = function()
 	{
